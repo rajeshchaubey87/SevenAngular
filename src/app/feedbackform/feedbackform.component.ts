60ae4da9a2a2dec825a82261c088f0bf29207679
unknown <rajeshchaubey87@gmail.com>
2019-04-26 01:24:56 +0530

    formWorking

--- src/app/feedbackform/feedbackform.component.ts
+++ src/app/feedbackform/feedbackform.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-feedbackform',
   templateUrl: './feedbackform.component.html',
   styleUrls: ['./feedbackform.component.css']
 })
-export class FeedbackformComponent implements OnInit {
+export class FeedbackformComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  onSubmit(form:NgForm){
+    console.log(form);
+    alert(form.control.value.fName);
+    alert(form.control.value.feedback);
   }
 
+  
 }
